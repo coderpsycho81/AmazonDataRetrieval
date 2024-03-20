9a3b202ec7d8a0436b3b7a2b0b74a7cace9d3d88
coderpsycho <101251156+coderpsycho81@users.noreply.github.com>
2024-03-20 11:10:26 +0300

    Add files via upload

---
+++ AmazonVeriAnalizi/AmazonVeriAnalizi/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace AmazonVeriAnalizi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.baslikBTN = new System.Windows.Forms.Button();
+            this.özellikBTN = new System.Windows.Forms.Button();
+            this.detayBTN = new System.Windows.Forms.Button();
+            this.resimBTN = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resimText = new System.Windows.Forms.RichTextBox();
+            this.detayText = new System.Windows.Forms.RichTextBox();
+            this.özellikText = new System.Windows.Forms.RichTextBox();
+            this.baslikText = new System.Windows.Forms.RichTextBox();
+            this.kaydetBTN = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(142)))), ((int)(((byte)(60)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "URL";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(254)))));
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.textBox1.Location = new System.Drawing.Point(111, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(432, 37);
+            this.textBox1.TabIndex = 1;
+            // 
+            // baslikBTN
+            // 
+            this.baslikBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(187)))), ((int)(((byte)(195)))));
+            this.baslikBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.baslikBTN.Enabled = false;
+            this.baslikBTN.FlatAppearance.BorderSize = 0;
+            this.baslikBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.baslikBTN.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.baslikBTN.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(41)))), ((int)(((byte)(70)))));
+            this.baslikBTN.Location = new System.Drawing.Point(12, 65);
+            this.baslikBTN.Name = "baslikBTN";
+            this.baslikBTN.Size = new System.Drawing.Size(93, 36);
+            this.baslikBTN.TabIndex = 2;
+            this.baslikBTN.Text = "BAŞLIK";
+            this.baslikBTN.UseVisualStyleBackColor = false;
+            this.baslikBTN.Click += new System.EventHandler(this.baslikBTN_Click);
+            // 
+            // özellikBTN
+            // 
+            this.özellikBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(187)))), ((int)(((byte)(195)))));
+            this.özellikBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.özellikBTN.Enabled = false;
+            this.özellikBTN.FlatAppearance.BorderSize = 0;
+            this.özellikBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.özellikBTN.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.özellikBTN.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(41)))), ((int)(((byte)(70)))));
+            this.özellikBTN.Location = new System.Drawing.Point(12, 113);
+            this.özellikBTN.Name = "özellikBTN";
+            this.özellikBTN.Size = new System.Drawing.Size(93, 36);
+            this.özellikBTN.TabIndex = 3;
+            this.özellikBTN.Text = "ÖZELLİK";
+            this.özellikBTN.UseVisualStyleBackColor = false;
+            this.özellikBTN.Click += new System.EventHandler(this.özellikBTN_Click);
+            // 
+            // detayBTN
+            // 
+            this.detayBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(187)))), ((int)(((byte)(195)))));
+            this.detayBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.detayBTN.Enabled = false;
+            this.detayBTN.FlatAppearance.BorderSize = 0;
+            this.detayBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.detayBTN.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.detayBTN.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(41)))), ((int)(((byte)(70)))));
+            this.detayBTN.Location = new System.Drawing.Point(12, 159);
+            this.detayBTN.Name = "detayBTN";
+            this.detayBTN.Size = new System.Drawing.Size(93, 36);
+            this.detayBTN.TabIndex = 4;
+            this.detayBTN.Text = "DETAYLI";
+            this.detayBTN.UseVisualStyleBackColor = false;
+            this.detayBTN.Click += new System.EventHandler(this.detayBTN_Click);
+            // 
+            // resimBTN
+            // 
+            this.resimBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(187)))), ((int)(((byte)(195)))));
+            this.resimBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.resimBTN.Enabled = false;
+            this.resimBTN.FlatAppearance.BorderSize = 0;
+            this.resimBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.resimBTN.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.resimBTN.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(41)))), ((int)(((byte)(70)))));
+            this.resimBTN.Location = new System.Drawing.Point(12, 206);
+            this.resimBTN.Name = "resimBTN";
+            this.resimBTN.Size = new System.Drawing.Size(93, 36);
+            this.resimBTN.TabIndex = 5;
+            this.resimBTN.Text = "RESİM";
+            this.resimBTN.UseVisualStyleBackColor = false;
+            this.resimBTN.Click += new System.EventHandler(this.resimBTN_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.resimText);
+            this.groupBox1.Controls.Add(this.detayText);
+            this.groupBox1.Controls.Add(this.özellikText);
+            this.groupBox1.Controls.Add(this.baslikText);
+            this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.groupBox1.Location = new System.Drawing.Point(111, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(432, 186);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "İşlemler";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(240)))), ((int)(((byte)(243)))));
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 25F);
+            this.label1.ForeColor = System.Drawing.Color.Brown;
+            this.label1.Location = new System.Drawing.Point(108, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 40);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Yükleniyor...";
+            this.label1.Visible = false;
+            // 
+            // resimText
+            // 
+            this.resimText.Location = new System.Drawing.Point(6, 23);
+            this.resimText.Name = "resimText";
+            this.resimText.Size = new System.Drawing.Size(420, 157);
+            this.resimText.TabIndex = 11;
+            this.resimText.Text = "";
+            this.resimText.Visible = false;
+            // 
+            // detayText
+            // 
+            this.detayText.Location = new System.Drawing.Point(6, 23);
+            this.detayText.Name = "detayText";
+            this.detayText.Size = new System.Drawing.Size(420, 157);
+            this.detayText.TabIndex = 10;
+            this.detayText.Text = "";
+            this.detayText.Visible = false;
+            // 
+            // özellikText
+            // 
+            this.özellikText.Location = new System.Drawing.Point(6, 23);
+            this.özellikText.Name = "özellikText";
+            this.özellikText.Size = new System.Drawing.Size(420, 157);
+            this.özellikText.TabIndex = 9;
+            this.özellikText.Text = "";
+            this.özellikText.Visible = false;
+            // 
+            // baslikText
+            // 
+            this.baslikText.Location = new System.Drawing.Point(6, 23);
+            this.baslikText.Name = "baslikText";
+            this.baslikText.Size = new System.Drawing.Size(420, 157);
+            this.baslikText.TabIndex = 0;
+            this.baslikText.Text = "";
+            this.baslikText.Visible = false;
+            // 
+            // kaydetBTN
+            // 
+            this.kaydetBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(120)))), ((int)(((byte)(81)))));
+            this.kaydetBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.kaydetBTN.FlatAppearance.BorderSize = 0;
+            this.kaydetBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kaydetBTN.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.kaydetBTN.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(254)))));
+            this.kaydetBTN.Location = new System.Drawing.Point(12, 248);
+            this.kaydetBTN.Name = "kaydetBTN";
+            this.kaydetBTN.Size = new System.Drawing.Size(532, 36);
+            this.kaydetBTN.TabIndex = 8;
+            this.kaydetBTN.Text = "KAYDET";
+            this.kaydetBTN.UseVisualStyleBackColor = false;
+            this.kaydetBTN.Click += new System.EventHandler(this.kaydetBTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(240)))), ((int)(((byte)(243)))));
+            this.ClientSize = new System.Drawing.Size(551, 289);
+            this.Controls.Add(this.kaydetBTN);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.resimBTN);
+            this.Controls.Add(this.detayBTN);
+            this.Controls.Add(this.özellikBTN);
+            this.Controls.Add(this.baslikBTN);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Amazon Veri Çekme / A. Kerim BOZTEPE";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button baslikBTN;
+        private System.Windows.Forms.Button özellikBTN;
+        private System.Windows.Forms.Button detayBTN;
+        private System.Windows.Forms.Button resimBTN;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox baslikText;
+        private System.Windows.Forms.RichTextBox özellikText;
+        private System.Windows.Forms.RichTextBox detayText;
+        private System.Windows.Forms.RichTextBox resimText;
+        private System.Windows.Forms.Button kaydetBTN;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AmazonVeriAnalizi/AmazonVeriAnalizi/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Threading;
+using System.IO;
+using System.Net.Http;
+
+namespace AmazonVeriAnalizi
+{
+    public partial class Form1 : Form
+    {
+        private ChromeDriver amazon;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Thread amazonThread;
+        private bool isActive = false;
+        private bool isDetay = false;
+        private bool isÖzellik = false;
+
+        #region Chrome Creat
+        private void chromeCreat()
+        {
+            // Options
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            options.AddArgument("--headless");
+
+            // Servics
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService(AppDomain.CurrentDomain.BaseDirectory);
+            service.HideCommandPromptWindow = true;
+
+            // Manager
+            amazon = new ChromeDriver(service, options);
+            amazon.Navigate().GoToUrl(textBox1.Text);
+            Thread.Sleep(500);
+
+            try
+            {
+                IWebElement element = amazon.FindElement(By.XPath("/html/body/div/div[1]/div[3]/div/div/form/div[1]/div/div/div[2]/div/div[2]/a"));
+                element.Click();
+
+                //*[@id="headingFour"]/h2/button
+            }
+            catch
+            {
+                
+            }
+
+            isActive = true;
+        }
+        #endregion
+
+        #region Başlat
+        private void StartAmazonThread()
+        {
+            amazonThread = new Thread(chromeCreat);
+            amazonThread.Start();
+        }
+
+        private void CheckIsActive()
+        {
+            while (true)
+            {
+                if (isActive)
+                {
+                    MessageBox.Show("İşlem tamamlandı!");
+
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        this.Cursor = Cursors.Default;
+
+                        label1.Visible = false;
+                        baslikBTN.Enabled = true;
+                        özellikBTN.Enabled = true;
+                        detayBTN.Enabled = true;
+                        resimBTN.Enabled = true;
+
+                    });
+
+                    break; 
+                }
+                Thread.Sleep(1000); 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("URL alanı boş olamaz!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            this.Cursor = Cursors.WaitCursor;
+            label1.Visible = true;
+            button2.Enabled = false;
+
+            StartAmazonThread();
+
+            Thread checkIsActiveThread = new Thread(CheckIsActive);
+            checkIsActiveThread.Start();
+        }
+        #endregion
+
+        #region Başlık Çek
+        private void baslikBTN_Click(object sender, EventArgs e)
+        {
+            groupBox1.Text = "Başlık Çek";
+            özellikText.Visible = false;
+            detayText.Visible = false;
+            resimText.Visible = false;
+
+            baslikText.Visible = true;
+
+            try
+            {
+                IWebElement element = amazon.FindElement(By.XPath("//*[@id=\"productTitle\"]"));
+                string text = element.Text;
+                baslikText.Text = text;
+            }
+            catch { MessageBox.Show("Bu Ürünün Başlığı Çekilemez", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+        #endregion
+
+        #region Özellik Çek
+        private void özellikBTN_Click(object sender, EventArgs e)
+        {
+            groupBox1.Text = "Özellik Çek";
+            baslikText.Visible = false;
+            detayText.Visible = false;
+            resimText.Visible = false;
+
+            özellikText.Visible = true;
+
+            try
+            {
+                IWebElement divElement = amazon.FindElement(By.XPath("//*[@id='productOverview_feature_div']/div"));
+                IWebElement tableElement = divElement.FindElement(By.XPath(".//table"));
+                IWebElement tbodyElement = tableElement.FindElement(By.XPath(".//tbody"));
+
+                IList<IWebElement> rows = tbodyElement.FindElements(By.XPath(".//tr"));
+
+                foreach (IWebElement row in rows)
+                {
+                    IList<IWebElement> cells = row.FindElements(By.TagName("td"));
+
+                    foreach (IWebElement cell in cells)
+                    {
+                        string cellText = cell.Text;
+                        özellikText.AppendText(cellText + "\t");
+                    }
+                    özellikText.AppendText(Environment.NewLine);
+                    isÖzellik = true;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Bu Ürünün Özellikleri Yok.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                isÖzellik = false;
+            }
+        }
+        #endregion
+
+        #region Detaylı Özellik Çek
+        private void detayBTN_Click(object sender, EventArgs e)
+        {
+            groupBox1.Text = "Detay Çek";
+            baslikText.Visible = false;
+            özellikText.Visible = false;
+            resimText.Visible = false;
+
+            detayText.Visible = true;
+
+            try
+            {
+                IWebElement divElement = amazon.FindElement(By.XPath("//*[@id='prodDetails']/div/div[1]/div[1]/div/div[2]/div/div"));
+                IWebElement tableElement = divElement.FindElement(By.TagName("table"));
+                IWebElement tbodyElement = tableElement.FindElement(By.TagName("tbody"));
+
+                IList<IWebElement> rows = tbodyElement.FindElements(By.TagName("tr"));
+
+                foreach (IWebElement row in rows)
+                {
+                    IList<IWebElement> headerCells = row.FindElements(By.TagName("th"));
+                    foreach (IWebElement headerCell in headerCells)
+                    {
+                        detayText.AppendText("Header: " + headerCell.Text + "\t");
+                    }
+
+                    IList<IWebElement> dataCells = row.FindElements(By.TagName("td"));
+                    foreach (IWebElement dataCell in dataCells)
+                    {
+                        detayText.AppendText("Data: " + dataCell.Text + "\t");
+                    }
+
+                    detayText.AppendText(Environment.NewLine);
+                    isDetay = true;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Bu Ürünün Detaylı Özellikleri Yok.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                isDetay = false;
+            }
+        }
+        #endregion
+
+        #region Resim Çek
+        private void resimBTN_Click(object sender, EventArgs e)
+        {
+            groupBox1.Text = "Resim İndirme ve Kaydetme";
+            baslikText.Visible = false;
+            özellikText.Visible = false;
+            detayText.Visible = false;
+
+            resimText.Visible = true;
+
+            IList<IWebElement> imgElements = amazon.FindElements(By.XPath("//*[@id='altImages']//img"));
+
+            foreach (IWebElement imgElement in imgElements)
+            {
+                string imgSrc = imgElement.GetAttribute("src");
+                resimText.AppendText(imgSrc + Environment.NewLine);
+            }          
+        }
+        #endregion
+
+        #region Kaydet Butonu
+        private async void kaydetBTN_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "Kaydedilecek yeri seçin";
+                dialog.RootFolder = Environment.SpecialFolder.Desktop;
+                DialogResult result = dialog.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
+                {
+                    string selectedPath = dialog.SelectedPath;
+                    string folderName = baslikText.Text;
+                    string folderPath = Path.Combine(selectedPath, folderName);
+
+                    string imageFolderPath = Path.Combine(selectedPath, "image");
+                    Directory.CreateDirectory(imageFolderPath);
+
+                    try
+                    {
+                        Directory.CreateDirectory(folderPath);
+                        string FilePath = Path.Combine(folderPath, "url.txt");
+                        string resimFilePath = Path.Combine(folderPath, "resimUrls.txt");
+
+                        if(isDetay)
+                        {
+                            string detaylıÖzellik = Path.Combine(folderPath, "detaylıÖzellikler.txt");
+
+                            #region Resim URL lerini Yazma
+                            using (StreamWriter writer = new StreamWriter(detaylıÖzellik))
+                            {
+                                foreach (string det in detayText.Lines)
+                                {
+                                    writer.WriteLine(det);
+                                }
+                            }
+                            #endregion
+                        }
+
+                        if (isÖzellik)
+                        {
+                            string özellik = Path.Combine(folderPath, "özellikleri.txt");
+
+                            #region Resim URL lerini Yazma
+                            using (StreamWriter writer = new StreamWriter(özellik))
+                            {
+                                foreach (string öz in özellikText.Lines)
+                                {
+                                    writer.WriteLine(öz);
+                                }
+                            }
+                            #endregion
+                        }
+
+                        #region URL DOSYA YAZMA
+                        using (StreamWriter writer = new StreamWriter(FilePath))
+                        {
+                            writer.Write(textBox1.Text);
+                        }
+                        #endregion
+
+                        #region Resim URL lerini Yazma
+                        using (StreamWriter writer = new StreamWriter(resimFilePath))
+                        {
+                            foreach (string url in resimText.Lines)
+                            {
+                                writer.WriteLine(url);
+                            }
+                        }
+                        #endregion
+
+                        #region Resimleri İndirme
+                        int count = 1;
+                        foreach (string url in resimText.Lines)
+                        {
+                            if (Uri.TryCreate(url, UriKind.Absolute, out Uri validUri))
+                            {
+                                string fileName = Path.GetFileName(url);
+                                string filePath = Path.Combine(imageFolderPath, $"{count}_{fileName}");
+
+                                using (HttpClient client = new HttpClient())
+                                {
+                                    byte[] imageBytes = await client.GetByteArrayAsync(validUri); // validUri kullan
+                                    File.WriteAllBytes(filePath, imageBytes);
+                                }
+                                count++;
+                            }
+                            else { }
+                        }
+                        #endregion
+
+                        #region image klasörünü taşıma
+                        try
+                        {
+                            if (!Directory.Exists(folderPath))
+                            {
+                                Directory.CreateDirectory(folderPath);
+                            }
+
+                            Directory.Move(imageFolderPath, Path.Combine(folderPath, "image"));
+
+                            MessageBox.Show("Fotoğraflar başarıyla indirildi ve kaydedildi.");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Hata oluştu: " + ex.Message);
+                        }
+                        #endregion
+                    }
+                    catch
+                    {
+                        MessageBox.Show("hata");
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
